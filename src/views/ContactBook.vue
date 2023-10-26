bece3b3aec110218adc44cc9b8b1c7501053e602
fridaytd <friday.infortech@gmail.com>
2023-10-26 13:40:26 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -39,6 +39,17 @@
                     <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact" />
+                <router-link
+                    :to="{
+                    name: 'contact.edit',
+                    params: { id: activeContact._id },
+                }"
+                >
+                    <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit"></i>
+                        Hiệu chỉnh
+                    </span>
+                </router-link>
             </div>
         </div>
     </div>
@@ -54,7 +65,7 @@ export default {
         InputSearch,
         ContactList,
     },
-// Đoạn mã xử lý đầy đủ sẽ trình bày bên dưới
+    // Đoạn mã xử lý đầy đủ sẽ trình bày bên dưới
     data() {
         return {
             contacts: [],
@@ -63,30 +74,30 @@ export default {
         };
     },
     watch: {
-    // Giám sát các thay đổi của biến searchText.
-    // Bỏ chọn phần tử đang được chọn trong danh sách.
+        // Giám sát các thay đổi của biến searchText.
+        // Bỏ chọn phần tử đang được chọn trong danh sách.
         searchText() {
             this.activeIndex = -1;
         },
     },
     computed: {
-    // Chuyển các đối tượng contact thành chuỗi để tiện cho tìm kiếm.
+        // Chuyển các đối tượng contact thành chuỗi để tiện cho tìm kiếm.
         contactStrings() {
             return this.contacts.map((contact) => {
                 const { name, email, address, phone } = contact;
                 return [name, email, address, phone].join("");
             });
-    },
-    // Trả về các contact có chứa thông tin cần tìm kiếm.
+        },
+        // Trả về các contact có chứa thông tin cần tìm kiếm.
         filteredContacts() {
             if (!this.searchText) return this.contacts;
-                return this.contacts.filter((_contact, index) =>
-                    this.contactStrings[index].includes(this.searchText)
+            return this.contacts.filter((_contact, index) =>
+                this.contactStrings[index].includes(this.searchText)
             );
         },
         activeContact() {
             if (this.activeIndex < 0) return null;
-                return this.filteredContacts[this.activeIndex];
+            return this.filteredContacts[this.activeIndex];
         },
         filteredContactsCount() {
             return this.filteredContacts.length;
